517782677dbe4e3d2b8eab0f4c6afa4af77c77b5
LinsThi <thiagolins13255@gmail.com>
2022-02-08 15:55:15 -0300

    Created FlatList of cities

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+
+import { Flatlist } from './modules/FlatCity/views/Flatlist';
 
 export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>SPARTANOS!</Text>
-    </View>
-  );
+  return <Flatlist />;
 }
