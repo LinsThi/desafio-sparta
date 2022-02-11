c7678c3bbca4150f2d18d5fae8947e0b8e97ed7a
LinsThi <thiagolins13255@gmail.com>
2022-02-11 19:56:59 -0300

    Updated constants

--- src/shared/constants/request/index.ts
+++ src/shared/constants/request/index.ts
@@ -3,7 +3,7 @@ export const FORMAT_JSON = 'json';
 export const COUNTRY = 'Brasil';
 
 export const BASE_URL_WEATHER = 'https://api.openweathermap.org/data/2.5';
-export const API_KEY_WEATHER = 'f29e6efb382a54f62b7f51fe1b50d829';
+export const API_KEY_WEATHER = 'YOUR_API_KEY';
 export const WEATHER = 'weather';
 export const ONE_CALL = 'onecall';
 export const UNITS_CELCIUS = 'metric';
